700eb263864364a6b12a84adf6a19a9c01e0f9b9
popeyeio <szl199083@126.com>
2018-12-20 11:52:13 +0800

    adjust tools

--- tools.go
+++ tools.go
@@ -391,15 +391,20 @@ func MustDo(f func() (interface{}, error), codes ...int) interface{} {
 }
 
 func Bytes2Str(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	sh := &reflect.StringHeader{
+		Data: bh.Data,
+		Len:  bh.Len,
+	}
+	return *(*string)(unsafe.Pointer(sh))
 }
 
 func Str2Bytes(s string) []byte {
 	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
-	bh := reflect.SliceHeader{
+	bh := &reflect.SliceHeader{
 		Data: sh.Data,
 		Len:  sh.Len,
 		Cap:  sh.Len,
 	}
-	return *(*[]byte)(unsafe.Pointer(&bh))
+	return *(*[]byte)(unsafe.Pointer(bh))
 }
